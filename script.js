25c30cc643a4ccceaf4a30f47e1c80c572b8ebc5
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-01 20:34:23 +0300

    Update script.js
    Testing saving

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 import {unitData} from "./unitData.js";
 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+  
+// Start file download.
+download("hello.txt",unitData);
+
 var app = angular.module("myConversionApp", []);
 app.controller("myCtrl", function ($scope) {
     //functions
