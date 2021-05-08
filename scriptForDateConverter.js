ccefda917b1690a82c68530f53009951a4ff06de
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-08 20:59:42 +0300

    Create scriptForDateConverter.js
    script for DateConverter page

---
+++ scriptForDateConverter.js
@@ -0,0 +1,32 @@
+
+var app = angular.module("myConversionApp", []);
+app.controller("myCtrl",  ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
+    
+    //functions
+    $scope.getHijriDate = function () {
+        $scope.error = false;
+        $.ajax({
+            url: "http://api.aladhan.com/gToH?date=" + $filter('date')($scope.inputDate, "dd-MM-yyyy"),
+            error: function (xhr) {
+                $scope.error = true;
+                $("#err").text("An error occured: " + xhr.status + " " + xhr.statusText);
+            },
+            success: function (result) {
+                console.log(result);
+                $scope.error = false;
+                $("#shortDate").text(result["data"]["hijri"]["date"])
+                $("#longDate").text(
+                    result["data"]["hijri"]["weekday"]["ar"]+ " " + 
+                    result["data"]["hijri"]["day"] + "-" +
+                    result["data"]["hijri"]["month"]["ar"]+ "-" +
+                    result["data"]["hijri"]["year"]
+                    )
+            }
+        });
+    }
+
+    //defs
+    $scope.inputDate = new Date('1989-03-07')
+    $scope.getHijriDate();
+    $scope.error = false;
+}]);
