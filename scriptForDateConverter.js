7d80d7aa6127f53d2b5f5d050aeffd43ab30eedf
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-18 17:48:49 +0300

    Hijri to Geo

--- scriptForDateConverter.js
+++ scriptForDateConverter.js
@@ -6,16 +6,16 @@ app.controller("myCtrl",  ['$scope', '$http', '$filter', function ($scope, $http
     $scope.getHijriDate = function () {
         $scope.error = false;
         $.ajax({
-            url: "https://api.aladhan.com/gToH?date=" + $filter('date')($scope.inputDate, "dd-MM-yyyy"),
+            url: "https://api.aladhan.com/gToH?date=" + $filter('date')($scope.gInputDate, "dd-MM-yyyy"),
             error: function (xhr) {
                 $scope.error = true;
-                $("#err").text("An error occured: " + xhr.status + " " + xhr.statusText);
+                $("#hErr").text("An error occured: " + xhr.status + " " + xhr.statusText);
             },
             success: function (result) {
                 console.log(result);
                 $scope.error = false;
-                $("#shortDate").text(result["data"]["hijri"]["date"])
-                $("#longDate").text(
+                $("#hShortDate").text(result["data"]["hijri"]["date"])
+                $("#hLongDate").text(
                     result["data"]["hijri"]["weekday"]["ar"]+ " " + 
                     result["data"]["hijri"]["day"] + "-" +
                     result["data"]["hijri"]["month"]["ar"]+ "-" +
@@ -25,8 +25,32 @@ app.controller("myCtrl",  ['$scope', '$http', '$filter', function ($scope, $http
         });
     }
 
+    $scope.getGeogDate = function () {
+        $scope.error = false;
+        $.ajax({
+            url: "https://api.aladhan.com/hToG?date=" + $filter('date')($scope.hInputDate, "dd-MM-yyyy"),
+            error: function (xhr) {
+                $scope.error = true;
+                $("#gErr").text("An error occured: " + xhr.status + " " + xhr.statusText);
+            },
+            success: function (result) {
+                console.log(result);
+                $scope.error = false;
+                $("#gShortDate").text(result["data"]["gregorian"]["date"])
+                $("#gLongDate").text(
+                    result["data"]["gregorian"]["weekday"]["en"]+ " " + 
+                    result["data"]["gregorian"]["day"] + "-" +
+                    result["data"]["gregorian"]["month"]["en"]+ "-" +
+                    result["data"]["gregorian"]["year"]
+                    )
+            }
+        });
+    }
+
     //defs
-    $scope.inputDate = new Date('1989-03-07')
+    $scope.gInputDate = new Date('1989-03-07')
+    $scope.hInputDate = new Date('1409-07-29')
     $scope.getHijriDate();
+    $scope.getGeogDate();
     $scope.error = false;
 }]);
