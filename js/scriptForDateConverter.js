26ca71b5ebe859b3ce1860ecd01ece6145de9a5f
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-05-19 00:33:58 +0300

    ehancments

--- js/scriptForDateConverter.js
+++ js/scriptForDateConverter.js
@@ -1,54 +1,33 @@
+$(function () {
+    $('#hpicker').hijriDatePicker({
+        hijri: true,
+        viewMode: 'months',
+        showSwitcher: false
+    });
+});
 
-var app = angular.module("myConversionApp", []);
-app.controller("myCtrl",  ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
-    
-    //functions
-    $scope.getHijriDate = function () {
-        $scope.error = false;
-        $.ajax({
-            url: "https://api.aladhan.com/gToH?date=" + $filter('date')($scope.gInputDate, "dd-MM-yyyy"),
-            error: function (xhr) {
-                $scope.error = true;
-                $("#hErr").text("An error occured: " + xhr.status + " " + xhr.statusText);
-            },
-            success: function (result) {
-                $scope.error = false;
-                $("#hShortDate").text(result["data"]["hijri"]["date"])
-                $("#hLongDate").text(
-                    result["data"]["hijri"]["weekday"]["ar"]+ " " + 
-                    result["data"]["hijri"]["day"] + "-" +
-                    result["data"]["hijri"]["month"]["ar"]+ "-" +
-                    result["data"]["hijri"]["year"]
-                    )
-            }
-        });
-    }
+$(function () {
+    $('#gpicker').hijriDatePicker({
+        hijri: false,
+        viewMode: 'months',
+        showSwitcher: false
+    });
+});
 
-    $scope.getGeogDate = function () {
-        $scope.error = false;
-        $.ajax({
-            url: "https://api.aladhan.com/hToG?date=" + $filter('date')($scope.hInputDate, "dd-MM-yyyy"),
-            error: function (xhr) {
-                $scope.error = true;
-                $("#gErr").text("An error occured: " + xhr.status + " " + xhr.statusText);
-            },
-            success: function (result) {
-                $scope.error = false;
-                $("#gShortDate").text(result["data"]["gregorian"]["date"])
-                $("#gLongDate").text(
-                    result["data"]["gregorian"]["weekday"]["en"]+ " " + 
-                    result["data"]["gregorian"]["day"] + "-" +
-                    result["data"]["gregorian"]["month"]["en"]+ "-" +
-                    result["data"]["gregorian"]["year"]
-                    )
-            }
-        });
-    }
+$("#hpicker").on('dp.change', function (arg) {
+    if (!arg.date) {
+        $("#gShortDate").html('');
+        return;
+    };
+    let date = arg.date;
+    $("#gShortDate").text("التاريخ الميلادي :" + date.format("YYYY/M/D") + " م");
+});
 
-    //defs
-    $scope.gInputDate = new Date('1989-03-07')
-    $scope.hInputDate = new Date('1409-07-29')
-    $scope.getHijriDate();
-    $scope.getGeogDate();
-    $scope.error = false;
-}]);
+$("#gpicker").on('dp.change', function (arg) {
+    if (!arg.date) {
+        $("#hShortDate").html('');
+        return;
+    };
+    let date = arg.date;
+    $("#hShortDate").text("التاريخ الهجري :" + date.format("iYYYY/iM/iD") + " هـ");
+});
