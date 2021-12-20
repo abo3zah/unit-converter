7ece02ee7fa58c3d76f704a7149f5274c53dc53f
abo3zah <31366659+abo3zah@users.noreply.github.com>
2021-12-21 01:04:34 +0300

    fixes

--- js/countryCode.js
+++ js/countryCode.js
@@ -1,12 +1,20 @@
 function showDropdownList(dropdownListID) {
-  $("#"+dropdownListID).addClass("show")
+  $("#" + dropdownListID).addClass("show")
 }
+
+$.ajax({
+  url: "https://www.google.com/search?q=%D9%85%D8%B3%D8%A7%D8%AD%D8%A9+%D8%A7%D9%84%D9%85%D9%85%D9%84%D9%83%D8%A9",
+  dataType: 'text',
+  success: function (data) {
+    console.log($(".Z0LcW").html());
+  }
+});
 
 function hideDropdownList(dropdownListID) {
-  $("#"+dropdownListID).removeClass("show")
+  $("#" + dropdownListID).removeClass("show")
 }
 
-$( "html" ).click(function(e) {
+$("html").click(function (e) {
   $(e.target)[0] == $('#country')[0] ? $("#countryList").addClass("show") : $("#countryList").removeClass("show");
 });
 
@@ -28,18 +36,34 @@ function filterFunction(dropdownListID, inputID) {
 
 const dataFetch = async () => {
   try {
-    const res = await d3.dsv(",", "csv/countryInfo.csv", d3.autoType);
+    const res = await d3.dsv(",", "csv/countryFullInfo.csv", d3.autoType);
     return res
   } catch (err) {
     console.error(err);
   }
 }
 
-function ulClickCountry(inputID,str,dropdownId){
+function ulClickCountry(inputID, str, dropdownId) {
   $(inputID).val(str);
   document.getElementById(dropdownId).classList.remove("show")
-  d3.dsv(",", "csv/countryInfo.csv", d3.autoType, (d)=> {
-    d['COUNTRY']==str? $("#callingCode").text("الرقم الاتصال: +" + d['COUNTRY CODE']).next().text("عدد السكان: "+d['POPULATION']).next().text("الاختصار: "+d['ISO CODES']) : null;
+  d3.dsv(",", "csv/countryFullInfo.csv", d3.autoType, (d) => {
+
+    if (d['name_ar'] == str) {
+      div = d3.select("#countryDiv");
+
+      div.selectAll("p, img").remove();
+
+      div.append("img").attr("src", d['flags/svg']).attr("width", 200).attr("height", 150).style('margin', '10px').attr('align', 'left').style('border', '1px solid black');
+      div.append("img").attr("src", d['coatOfArms/svg']).attr("width", 200).attr("height", 150).style('margin', '10px').attr('align', 'left').style('border', '1px solid black');
+      div.append('p').style("text-align", "right").style('margin-top', '10px').html("<b>الاسم الرسمي:</b> " + d['name/official_ar']);
+      div.append('p').style("text-align", "right").html("<b>القارة: </b>" + d['region_ar']);
+      div.append('p').style("text-align", "right").html("<b>العاصمة: </b>" + d['capital_ar']);
+      div.append('p').style("text-align", "right").html("<b>المساحة (كيلومتر مربع): </b>" + d['area'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      div.append('p').style("text-align", "right").html("<b>عدد السكان: </b>" + d['population'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      div.append('p').style("text-align", "right").html("<b>الرقم الاتصال:</b> +" + d['Phone Code']);
+      div.append('p').style("text-align", "right").html("<b>الاختصار: </b>" + d['cca3']);
+    }
+
   })
 }
 
@@ -54,6 +78,6 @@ dataFetch().then((data) => {
     .data(data)
     .enter().append("ul")
     .attr('onclick', "ulClickCountry('#country',$(this).text(),'countryList')")
-    .text((d) => d['COUNTRY']);
+    .text((d) => d['name_ar']);
 
 });
