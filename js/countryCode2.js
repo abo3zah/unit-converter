16b97b031680def2360752d39a926e00c42e4885
abo3zah <31366659+abo3zah@users.noreply.github.com>
2022-02-09 17:16:11 +0300

    updated

--- js/countryCode2.js
+++ js/countryCode2.js
@@ -1,18 +1,28 @@
+//adding the svg
 const svg = d3.select("#main")
   .append('svg')
 
 const width = svg.attr('width');
 const height = svg.attr('height');
 
-const projection = d3.geoNaturalEarth1();
-const pathGenerator = d3.geoPath().projection(projection)
+//selection of projection
+const projection = d3.geoNaturalEarth1()
+
+//selection of graticule
+const graticule = d3.geoGraticule()
+  .step([10, 10]);
+
+const pathGenerator = d3.geoPath().projection(projection);
+
+//color scale
 const colorScale = d3.scaleOrdinal(d3.schemeAccent)
 /*const colorScale = d3.scaleLinear()
   .domain([0,240])
   .range(['green','yellow'])*/
 
 const g = svg.append('g')
-  .attr('transform','translate(' + (width/2) + ',' + (height /2) +')');
+  //.attr('transform','translate(' + (960/2) + ',' + (480 /2) +')');
+
 
 g.append('path')
   .attr('class', 'sphere')
@@ -20,24 +30,34 @@ g.append('path')
     type: 'Sphere'
   }));
 
+//zooming feature
 svg.call(d3.zoom().on('zoom', e => {
   g.attr('transform', (transform = e.transform));
 }));
 
+//data fetching
 Promise.all([
   d3.json('https://unpkg.com/world-atlas@2.0.2/countries-50m.json'),
 ]).then(([topology]) => {
 
   const countries = topojson.feature(topology, topology.objects.countries);
 
-  console.log(countries);
+  //add graticules
+  g.selectAll('path.graticules')
+    .data([graticule()])
+    .enter()
+    .append('path')
+      .attr('d', d => pathGenerator(d))
+      .attr('class','graticules')
 
-  g.selectAll('path').data(countries.features)
+  //add countries
+  g.selectAll('path.country').data(countries.features)
     .enter()
     .append('path')
       .attr('d', d => pathGenerator(d))
       .attr('class', 'country')
-      .attr('fill', d => colorScale(d.id))
+      //.attr('fill', d => colorScale(d.id))
+      .attr('fill', 'lightyellow')
       .append('title')
         .text(d => d.properties.name)
 })
---
+++ js/countryCode3.js
@@ -0,0 +1,132 @@
+function invertColor(hex, bw) {
+
+  if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+  }
+
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+  }
+
+  var r = parseInt(hex.slice(0, 2), 16),
+      g = parseInt(hex.slice(2, 4), 16),
+      b = parseInt(hex.slice(4, 6), 16);
+
+  if (bw) {
+      return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+          ? '#000000'
+          : '#FFFFFF';
+  }
+
+  // invert color components
+  r = (255 - r).toString(16);
+  g = (255 - g).toString(16);
+  b = (255 - b).toString(16);
+
+  // pad each with zeros and return
+  return "#" + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+  len = len || 2;
+  var zeros = new Array(len).join('0');
+  return (zeros + str).slice(-len);
+}
+
+
+//adding the svg
+const svg = d3.select("#main")
+  .append('svg')
+
+const width = svg.attr('width');
+const height = svg.attr('height');
+
+//selection of projection
+const projection = d3.geoEquirectangular()
+
+//selection of graticule
+const graticule = d3.geoGraticule()
+  .step([10, 10]);
+
+const pathGenerator = d3.geoPath().projection(projection);
+
+//color scale
+const colorScale = d3.scaleOrdinal(d3.schemeSet3 )
+/*const colorScale = d3.scaleLinear()
+  .domain([0,240])
+  .range(['green','yellow'])*/
+
+const g = svg.append('g')
+  //.attr('transform','translate(' + (960/2) + ',' + (480 /2) +')');
+
+//zooming feature
+svg.call(d3.zoom().on('zoom', e => {
+  g.attr('transform', (transform = e.transform));
+}));
+
+function draw(yaw, countries, borders){
+
+  projection.rotate([yaw])
+
+  g.selectAll("*").remove();
+
+  g.append('path')
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({
+    type: 'Sphere'
+  }));
+
+  //add graticules
+  g.selectAll('path.graticules')
+    .data([graticule()])
+    .enter()
+    .append('path')
+      .attr('d', d => pathGenerator(d))
+      .attr('class','graticules')
+
+  //add countries
+  g.selectAll('path.country').data(countries.features)
+    .enter()
+    .append('path')
+      .attr('d', d => pathGenerator(d))
+      .attr('class', 'country')
+      .attr('fill', d => colorScale(d.properties.MAPCOLOR9))
+      //.attr('fill', 'lightyellow')
+      .append('title')
+        .text(d=> d.properties.NAME_AR +
+          "\nعدد السكان: " + d.properties.POP_EST.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+
+  //add inner borders
+  g.selectAll('path.borders').data(borders.features)
+    .enter()
+    .append('path')
+      .attr('d', d => pathGenerator(d))
+      .attr('class', 'borders');
+
+  g.selectAll('text.countryName').data(countries.features)
+    .enter()
+    .append('text')
+      .attr('class','countryName')
+      .attr('unselectable','on')
+      .attr('x', d => projection(d3.geoCentroid(d))[0])
+      .attr('y', d => projection(d3.geoCentroid(d))[1])
+      .attr('font-size',d => (Math.exp(d3.geoArea(d)) * 2) +'px')
+      .text(d => d.properties.LABELRANK < 4 ? d.properties.NAME_AR : "");
+}
+
+//data fetching
+Promise.all([
+  d3.json('json/countries.geojson'),
+  d3.json('json/borders.geojson')
+]).then(([countriesInfo, borders]) => {
+
+  var countries = countriesInfo;
+
+  console.log(countries);
+  
+  draw(-10, countries, borders)
+})
