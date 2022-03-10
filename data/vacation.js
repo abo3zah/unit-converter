66cd0e66e0402b88fc8263d81e498e5bfd12125a
abo3zah <31366659+abo3zah@users.noreply.github.com>
2022-03-10 19:56:14 +0300

    update

---
+++ data/vacation.js
@@ -0,0 +1,92 @@
+vacationData = [
+    {
+        "name":"إجازة",
+        "start": "06-Jan-2022",
+        "end": "15-Jan-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "02-Feb-2022",
+        "end": "02-Feb-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "22-Feb-2022",
+        "end": "22-Feb-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "23-Feb-2022",
+        "end": "24-Feb-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "11-Mar-2022",
+        "end": "19-Mar-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"عيد الفطر",
+        "start": "25-Apr-2022",
+        "end": "07-May-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "25-May-2022",
+        "end": "26-May-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "15-Jun-2022",
+        "end": "16-Jun-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة",
+        "start": "01-July-2022",
+        "end": "01-July-2022",
+        "style": "background-color:yellow;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "09-Jan-2022",
+        "end": "09-Jan-2022",
+        "style": "background-color:#87CEFA;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "13-Mar-2022",
+        "end": "13-Mar-2022",
+        "style": "background-color:#87CEFA;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "01-May-2022",
+        "end": "04-May-2022",
+        "style": "background-color:#87CEFA;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "08-Jul-2022",
+        "end": "13-Jul-2022",
+        "style": "background-color:#87CEFA;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "22-Sep-2022",
+        "end": "22-Sep-2022",
+        "style": "background-color:#87CEFA;",
+    },
+    {
+        "name":"إجازة ارامكو",
+        "start": "13-Nov-2022",
+        "end": "13-Nov-2022",
+        "style": "background-color:#87CEFA;",
+    }
+]
---
+++ js/calendar.js
@@ -0,0 +1,129 @@
+const getDatesBetween = (startDate, endDate, includeEndDate) => {
+    const dates = [];
+    const currentDate = startDate;
+    while (currentDate < endDate) {
+        dates.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+    if (includeEndDate) dates.push(endDate);
+    return dates;
+};
+
+const createArr = (num) => {
+    var arr = [];
+    if (num == 0){return arr};
+    for (let i = 0; i < num-1; i++) {
+        arr.push([' ']);        
+    }
+    arr.push(["*"])
+    return arr;
+};
+
+const twoDigit = (str) => {
+    return str.length == 1 ? "0" + str : str;
+}
+
+var replaceDigits = function (str) {
+    return str.replace(/\d(?=[^<>]*(<|$))/g, function ($0) {return map[$0]});
+}
+
+var selectedYear = new Date().getFullYear();
+var startDate = new Date(selectedYear + "-01-01");
+var endDate = new Date(selectedYear + "-12-31");
+var dates = getDatesBetween(startDate, endDate, true);
+var hYear = [];
+var map = [
+    "&#1632;", "&#1633;", "&#1634;", "&#1635;", "&#1636;",
+    "&#1637;", "&#1638;", "&#1639;", "&#1640;", "&#1641;"
+]
+
+const weekday = ["أ","ث","ث","ر","خ","ج","س"];
+const MonthName = ["يناير","فبراير","مارس","أبريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أوكتوبر","نوفمبر","ديسمبر"];
+const hMonthName = ["محرم","صفر","ربيع أول","ربيع ثاني","جمادى أول","جمادى ثاني","رجب","شعبان","رمضان","شوال","ذو القعدة","ذو الحجة"]
+const hColors = ['#32CD32', '#0000CD', '#FFA500'];
+
+for (let i = 0; i <= 11; i++) {
+
+    d3.select('[data-month="'+ (i+1) +'"]')
+        .selectAll('span.dayName')
+            .data(weekday)
+            .enter()
+            .append('span')
+                .attr('class','dayName')
+                .text(d => d)
+
+    let hMonths = [];
+    d3.select('[data-month="'+ (i+1) +'"]')
+        .selectAll('div.day')
+            .data(d3.merge([createArr((new Date(selectedYear + "-" + (i+1) + "-01")).getDay()),d3.filter(dates,d => d.getMonth() == i)]))
+            .enter()
+            .append('div')
+                .attr('class', (d)=> {
+                    if (d == " "){ return "empty"};
+                    if (d =="*") { return "lastEmpty"};
+                    classes = 'day ';
+                    d.toDateString() == new Date().toDateString()? classes+="today ":null;
+                    d.getDay()>4? classes+="weekEnd " : classes+='weekDay ';
+                    d.getDay()==0? classes+="sundays " : null;
+                    d.getDate()==1? classes+="startOfMonth " : null;
+                    let m = moment(d.toISOString().slice(0,10), 'YYYY/MM/DD');
+                    m.format('iMM')==9?classes+="ramadan " : null;
+                    return classes;
+                })
+                .attr('style',(d) => {
+
+                    if (d == " "){ return ""};
+                    if (d =="*") { return ""};
+
+                    let styleString = ""
+
+                    let m = moment(d.toISOString().slice(0,10), 'YYYY/MM/DD');
+                    for (let z = 0; z < vacationData.length; z++){
+                        if(m.isBetween(vacationData[z].start, vacationData[z].end, undefined, '[]')){
+                            styleString = vacationData[z].style;
+                        }
+                    }
+                    return styleString;
+                })
+                .attr('data-date', d => d.toLocaleString().split(',')[0])
+                .html((d) => {
+                    let text = '';
+                    if (d == " " || d == "*"){
+                        text = " ";
+                    }else{
+                        text = d.getDate();
+                        let m = moment(d.toISOString().slice(0,10), 'YYYY/MM/DD');
+                        hMonths.includes(m.format('iMMM'))?null: hMonths.push(m.format('iMMM'));
+                        hYear.includes(m.format('iYYYY'))?null: hYear.push(m.format('iYYYY'));
+                    }
+                    return text;
+                })
+                .append('span')
+                    .html((d) => {
+                        let text = '';
+                        if (d == " " || d == "*"){
+                            text = " ";
+                        }else{
+                            let m = moment(d.toISOString().slice(0,10), 'YYYY/MM/DD');
+                            text = '<span style="color:' + hColors[hMonths.indexOf(m.format('iMMM'))] + '">'+ replaceDigits(String(m.iDate())) +'</span>';
+                        }
+                        return text;
+                    });
+
+    d3.select('[data-month="'+ (i+1) +'"] div.monthHeader')
+        .text(() => {
+            return MonthName[i];
+        })
+        .append('span')
+            .html(() => {
+                let text = '';
+                for (let j = 0; j<hMonths.length;j++){
+                    text += '<span style="color:'+ hColors[j] + '">'+ hMonths[j] +'</span>'
+                    j == (hMonths.length - 1) ? null: text += ' - '
+                }
+                return text;
+            })
+}
+
+let str = 'تقويم سنة ' + selectedYear + ' (' + replaceDigits(hYear.reduce((p,c)=> p + " - " + c)) + ")";
+d3.select('.mainBox > h2').html(str);
