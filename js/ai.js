4b1eb53f4772650572bbec1c710d05178de68dbb
abo3zah <31366659+abo3zah@users.noreply.github.com>
2022-04-23 00:52:24 +0300

    update

--- js/ai.js
+++ js/ai.js
@@ -1,231 +1,275 @@
-var model;
-var data;
-var label;
+let data, xAxis, yAxis, AImodel, uniqueClass, messageSize;
 
-async function saveModel() {
-    //save model
-    await model.save('downloads://IRIS');
+function preload() {
+     //download Data
+     data = loadTable('https://raw.githubusercontent.com/curran/data/gh-pages/all/iris_iris.csv', 'csv', 'header');
 }
 
-async function testModel() {
+async function setup() {
+     noCanvas()
+     noLoop()
 
-    yPred = tf.tensor2d([[+document.getElementById('PL').value, +document.getElementById('PW').value]])
+     messageSize = 0;
 
-    let result = await model.predict(yPred).dataSync()
+     //unhide the main div
+     select('#main').addClass('grid')
+     select('#main').removeClass('hidden')
 
-    result = result.map(x => Math.round(x))
+     //append data to dropdown
+     xAxis = select('#x-axis')
+     yAxis = select('#y-axis')
 
-    document.getElementById('result').innerText = result[0]==1?'setosa':result[2]==1?'versicolor':'virginca'
+     for (col of data.columns) {
+          if (col == 'Class') {
+               continue;
+          }
+          xAxis.option(col);
+          yAxis.option(col);
+     }
 
+     //select columns
+     xAxis.selected('Petal Length');
+     yAxis.selected('Petal Width');
+
+     //plot grapgh
+     await preparingForPloting();
+
+     //add events
+     xAxis.changed(preparingForPloting);
+     yAxis.changed(preparingForPloting);
+
+
+     //creating the model
+     await createModel()
+
+     //train model
+     trainmodel()
+
+}
+
+async function preparingForPloting() {
+
+     tf.tidy(() => {
+          //extract unique classes
+          uniqueClass = tf.unique(data.getColumn('Class')).values.arraySync();
+
+          //prepare the data to plot
+          let values = [];
+          for (let index = 0; index < uniqueClass.length; index++) {
+               let RegEx = new RegExp(uniqueClass[index], 'g')
+               values[index] = {
+                    x: data.matchRows(RegEx, 4).map(d => d.getNum(xAxis.value())),
+                    y: data.matchRows(RegEx, 4).map(d => d.getNum(yAxis.value())),
+                    mode: "markers",
+                    name: uniqueClass[index],
+                    type: 'scatter',
+               };
+          }
+
+          // Define Layout
+          var layout = {
+               xaxis: {
+                    title: xAxis.value()
+               },
+               yaxis: {
+                    title: yAxis.value()
+               },
+               title: {
+                    text: xAxis.value() + " vs. " + yAxis.value(),
+                    font: {
+                         size: 30
+                    }
+               },
+               paper_bgcolor: "#fffbeb",
+               plot_bgcolor: "#fffbeb"
+          };
+
+
+          //toggle options based on selections
+          toggleStatusForOptions()
+
+          //plot grapgh
+          plotGrapgh("myChart", values, layout)
+     })
+}
+
+function plotGrapgh(containerID, values, layout) {
+
+     // Display using Plotly
+     Plotly.newPlot(containerID, values, layout, {
+          displaylogo: false,
+          staticPlot: false,
+          responsive: true
+     });
+
+     // trainmodel()
+}
+
+function toggleStatusForOptions() {
+     for (option of selectAll('select[name*="-axis"] option')) {
+          if (option.value() == xAxis.value()) {
+               yAxis.disable(option.value())
+          } else if (option.value() == yAxis.value()) {
+               xAxis.disable(option.value())
+          } else {
+               option.removeAttribute('disabled')
+          }
+     }
 }
 
 async function createModel() {
-    // Create a sequential model
-    const newModel = tf.sequential();
+     tf.tidy(async () => {
+          //create a model
+          AImodel = tf.sequential();
 
-    // Add a single input layer
-    newModel.add(tf.layers.dense({ 
-        inputShape: [2],
-        activation: "sigmoid", //'elu'|'hardSigmoid'|'linear'|'relu'|'relu6'| 'selu'|'sigmoid'|'softmax'|'softplus'|'softsign'|'tanh'|'swish'|'mish'
-        units: 5,
-        name: "Input_Layer",
-        //dtype: 'float32',//'float32'|'int32'|'bool'|'complex64'|'string'
-    }));
+          //add input layer
+          AImodel.add(tf.layers.dense({
+               inputShape: [2],
+               //alternatives for activation are 'elu'|'hardSigmoid'|'linear'|'relu'|'relu6'| 'selu'|'sigmoid'|'softmax'|'softplus'|'softsign'|'tanh'|'swish'|'mish'
+               activation: "sigmoid",
+               units: 5,
+               name: "Input_Layer",
+               //dtype: 'float32',//'float32'|'int32'|'bool'|'complex64'|'string'
+          }));
 
-    // create hidden layers
-    newModel.add(tf.layers.dense({ units: 20 }));
-    newModel.add(tf.layers.dense({ units: 10 }));
+          //add hidden layers
+          AImodel.add(tf.layers.dense({
+               units: 20,
+               name: "First_Hidden_Layer"
+          }))
+          AImodel.add(tf.layers.dense({
+               units: 20,
+               name: "Second_Hidden_Layer"
+          }))
 
-    // Add an output layer
-    newModel.add(tf.layers.dense({ 
-        units: 3,
-        name: "Output_Layer"
-    }))
+          //add output layers
+          AImodel.add(tf.layers.dense({
+               units: 3,
+               name: "Output_Layer"
+          }))
 
-    // Prepare the model for training.
-    await newModel.compile({ loss: "meanSquaredError", optimizer: tf.train.adam(.06) });
+          //Prepare the model for tarining
+          await complieModel();
 
-    newModel.summary()
-
-    return newModel;
+          //Print a summary of the model
+          AImodel.summary()
+     })
 }
 
+async function trainmodel(epochs = 50) {
 
-function convertToTensor(data) {
+     //creating the xs
+     x = tf.tidy(() => tf.tensor2d(data.getRows().map(d => [d.getNum(xAxis.value()), d.getNum(yAxis.value())])))
 
-    xAxis = document.getElementById('x-axis').value;
-    yAxis = document.getElementById('y-axis').value;
+     //creating and encoding the ys
+     y = tf.tidy(() => tf.oneHot(tf.tensor1d(data.getColumn('Class').map(d => uniqueClass.indexOf(d)), 'int32'), uniqueClass.length))
 
-    return tf.tidy(() => {
-        // Step 1. Shuffle the data
-        tf.util.shuffle(data);
+     //printing the input and output shape
+     // print(x.shape, y.shape)
 
-        // Step 2. Convert data to Tensor
-        const inputs = data.map(d => [d[xAxis], d[yAxis]])
-        const labels = data.map(d => [
-            d[label] == "setosa" ? 1 : 0,
-            d[label] == "virginica" ? 1 : 0,
-            d[label] == "versicolor" ? 1 : 0
-        ]);
+     // Define Layout
+     var layout = {
+          xaxis: {
+               title: 'epochs'
+          },
+          yaxis: {
+               title: 'Loss'
+          },
+          title: {
+               text: "epochs vs. Loss",
+               font: {
+                    size: 30
+               }
+          },
+          paper_bgcolor: "#fffbeb",
+          plot_bgcolor: "#fffbeb"
+     };
 
-        const inputTensor = tf.tensor2d(inputs);
-        const labelTensor = tf.tensor2d(labels);
+     let xs = [];
+     let ys = [];
+     let values = [];
 
-        return {
-            inputs: inputTensor,
-            labels: labelTensor,
-        }
-    });
+     await tf.tidy(() => {
+          AImodel.fit(x, y, {
+               epochs,
+               shuffle: true,
+               validationSplit: 0.1,
+               callbacks: {
+                    onEpochEnd: async (epoch, logs) => {
+                         xs.push(epoch);
+                         ys.push(logs.loss)
+                         values = [{
+                              x: xs,
+                              y: ys,
+                              type: 'scatter'
+                         }];
+                         plotGrapgh("myChart2", values, layout)
+                    }
+               }
+          });
+     })
 }
 
-async function trainModel(){
+function showMessage(message = 'hello'){
 
-    // Convert the data to a form we can use for training.
-    const tensorData = convertToTensor(data);
-    const { inputs, labels } = tensorData;
+     let div = createDiv(message);
+     div.parent('alertsContainer');
+     div.addClass('bg-red-400 text-white font-bold block m-3 rounded');
+     div.center();
+     
+     div.position(0, 0+messageSize,'static');
 
-    if (model != undefined){
-        console.log('a Model is already defined and trained');
-        // return null;
-    }
-
-
-    //create Model
-    model = await createModel();
-
-    const epochs = document.getElementById('epochs').value;
-
-    // Define Layout
-    var layout = {
-        xaxis: {
-            title: 'epochs'
-        },
-        yaxis: {
-            title: 'Loss'
-        },
-        title: {
-            text: "epochs vs. Loss",
-            font: {
-                size: 30
-            }
-        },
-        paper_bgcolor: "#fffbeb",
-        plot_bgcolor: "#fffbeb"
-    };
-
-    let xs = [];
-    let ys = [];
-    let values = [];
-
-    await model.fit(inputs, labels, {
-        epochs,
-        shuffle: true,
-        callbacks: {
-            onEpochEnd: async (epoch, logs) => {
-                xs.push(epoch);
-                ys.push(logs.loss)
-                values = [{
-                    x:xs,
-                    y:ys,
-                    type: 'scatter'
-                }];
-                Plotly.newPlot("myChart2", values, layout, {
-                    displaylogo: false,
-                    responsive: true
-                })
-            }
-          }
-    });
+     messageSize += div.height + 10
+     setTimeout(() => {
+          messageSize -= div.height
+          div.remove()
+     }, 5000);
 }
 
-async function loadModel() {
+async function loadModelFiles() {
 
-    const jsonUpload = document.getElementById('json-upload');
-    const weightsUpload = document.getElementById('weights-upload');
+     let jsonUpload = select('#json-upload').elt.files;
+     let weightsUpload = select('#weights-upload').elt.files;
 
-    //TODO: check if empty send a notification
+     if (jsonUpload.length == 0 || weightsUpload.length == 0) {
+          showMessage('No files');
+          return null
+     }
 
-    model = await tf.loadLayersModel(
-        tf.io.browserFiles([jsonUpload.files[0], weightsUpload.files[0]])
-    );
-    model.summary();
+     AImodel = await tf.loadLayersModel(
+          tf.io.browserFiles([jsonUpload[0], weightsUpload[0]])
+     );
 
+     AImodel.summary();
+
+     complieModel();
 }
 
-function drawChart(data) {
-
-    xAxis = document.getElementById('x-axis').value;
-    yAxis = document.getElementById('y-axis').value;
-
-    let uniqueClass = data.map(d => d[label]).filter((element, index, arr) => arr.indexOf(element) === index)
-
-    let values = [];
-
-    for (let index = 0; index < uniqueClass.length; index++) {
-        values[index] = {
-            x: data.filter(d => d[label] == uniqueClass[index]).map(d => d[xAxis]),
-            y: data.filter(d => d[label] == uniqueClass[index]).map(d => d[yAxis]),
-            mode: "markers",
-            name: uniqueClass[index],
-            type: 'scatter',
-        };
-    }
-
-    // Define Layout
-    var layout = {
-        xaxis: {
-            title: xAxis
-        },
-        yaxis: {
-            title: yAxis
-        },
-        title: {
-            text: xAxis + " vs. " + yAxis,
-            font: {
-                size: 30
-            }
-        },
-        paper_bgcolor: "#fffbeb",
-        plot_bgcolor: "#fffbeb"
-    };
-
-    // Display using Plotly
-    Plotly.newPlot("myChart", values, layout, {
-        displaylogo: false,
-        staticPlot: false,
-        responsive: true
-    });
+async function complieModel() {
+     //Prepare the model for tarining
+     AImodel.compile({
+          //alternatives for loss are absoluteDifference, computeWeightedLoss, cosineDistance, hingeLoss, huberLoss, 
+          //logLoss, meanSquaredError, sigmoidCrossEntropy, softmaxCrossEntropy, categoricalCrossentropy
+          loss: "meanSquaredError",
+          //alternatives for optimizer are tf.train.sgd, tf.train.momentum, tf.train.adagrad, tf.train.adadelta, tf.train.adam, tf.train.adamax, tf.train.rmsprop
+          optimizer: tf.train.adam(.05),
+          //alternative for metrics are 
+          metrics: ['acc']
+     });
 }
 
-async function getData() {
-    let csvUrl = 'https://raw.githubusercontent.com/curran/data/gh-pages/all/iris_iris.csv';
+async function predictType(arr) {
+     tf.tidy(() => {
 
-    const DataResponse = await d3.dsv(',', csvUrl, d3.autoType);
+          let yPred = tf.tensor2d(arr);
 
-    label = DataResponse.columns.slice(-1);
+          let result = AImodel.predict(yPred).argMax(1).arraySync()[0];
 
-    for (col of DataResponse.columns.slice(0, -1)) {
-        d3.select('#x-axis').append('option')
-            .attr('value', col)
-            .text(col)
-        
-        d3.select('#y-axis').append('option')
-            .attr('value', col)
-            .text(col)
-    }
-
-    d3.select('#x-axis').property('value', 'Petal Length');
-    d3.select('#y-axis').property('value', 'Petal Width');
-
-    return DataResponse;
+          document.getElementById('result').innerText = uniqueClass[result]
+     })
 }
 
-async function run() {
-    // Load and plot the original input data that we are going to train on.
-    data = await getData();
-
-
-    drawChart(data);
+async function saveModelFiles() {
+     //save model
+    await AImodel.save('downloads://IRIS');
 }
-
-document.addEventListener('DOMContentLoaded', run);
